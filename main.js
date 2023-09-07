7a64bbe3f15f000a4bea34a81dfe7430cf145223
thiagoserafimdasilva <105942396+thiagoserafimdasilva@users.noreply.github.com>
2023-09-06 22:20:20 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-function tocaSom (idElementoAudio) {
-  document.querySelector(idElementoAudio).play();
+function tocaSom (selectorAudio) {
+  const elemento = document.querySelector(selectorAudio).play();
+
+
+    if(elemento && elemento.localName === 'audio') {
+       elemento.play();     
+   }  
+   else { 
+        //alert('Elemento não encontrado ou seletor inválido');
+        console.log('Elemento ou seletor não encontrado!')
+    } 
 }
 
 const listadeTeclas = document.querySelectorAll('.tecla');
@@ -8,20 +17,23 @@ const listadeTeclas = document.querySelectorAll('.tecla');
 for (let contador = 0; contador < listadeTeclas.length; contador++ ) {
 
     const tecla = listadeTeclas[contador];
-
     const instrumento = tecla.classList[1];
-
     const idAudio = `#som_${instrumento}`;
 
     tecla.onclick = function () { 
-        tocaSom(idAudio)
-    };
-
-    tecla.onkeydown = function () {
-        tecla.classList.add('ativa');
+        tocaSom(idAudio);
     }
 
-    tecla,onkeyup = function () {
+    tecla.onkeydown = function (evento) {
+
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = function () {
         tecla.classList.remove('ativa');
     }
 
